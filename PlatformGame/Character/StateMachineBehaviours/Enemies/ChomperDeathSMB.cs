30e1e24b5ef458ee89a9879c1ebefb090a9003b3
Creeper0809 <47589141+Creeper0809@users.noreply.github.com>
2021-07-13 14:50:28 +0900

    Add files via upload

---
+++ PlatformGame/Character/StateMachineBehaviours/Enemies/ChomperDeathSMB.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gamekit2D
+{
+    public class ChomperDeathSMB : SceneLinkedSMB<EnemyBehaviour>
+    {
+        public override void OnSLStateEnter (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            m_MonoBehaviour.DisableDamage ();
+        }
+
+        public override void OnSLStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            m_MonoBehaviour.gameObject.SetActive(false);
+            item.electronQuantity += 2;
+            item.atomQuantity+= 2;
+        }
+    }
+}
